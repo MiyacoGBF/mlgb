bf47086c526ccce174135d32d4d6ad9a455263ae
MiyacoGBF <miyaco.pl@gmail.com>
2020-07-06 18:11:39 +0900

    [Theme] support old browsers (js)

--- themes/hello-friend/static/js/main.js
+++ themes/hello-friend/static/js/main.js
@@ -1,25 +1,25 @@
 // Menu
-const container = document.querySelector('.container');
-const menu = document.querySelector('.menu');
-const mobileMenuTrigger = document.querySelector('.menu-trigger');
-const desktopMenu = document.querySelector('.menu__inner--desktop');
-const desktopMenuTrigger = document.querySelector('.menu__sub-inner-more-tigger');
-const menuMore = document.querySelector('.menu__sub-inner-more');
-const mobileQuery = getComputedStyle(document.body).getPropertyValue('--phoneWidth');
-const isMobile = () => window.matchMedia(mobileQuery).matches;
-const isMobileMenu = () => {
+var container = document.querySelector('.container');
+var menu = document.querySelector('.menu');
+var mobileMenuTrigger = document.querySelector('.menu-trigger');
+var desktopMenu = document.querySelector('.menu__inner--desktop');
+var desktopMenuTrigger = document.querySelector('.menu__sub-inner-more-tigger');
+var menuMore = document.querySelector('.menu__sub-inner-more');
+var mobileQuery = getComputedStyle(document.body).getPropertyValue('--phoneWidth');
+var isMobile = function() { return window.matchMedia(mobileQuery).matches };
+var isMobileMenu = function() {
   mobileMenuTrigger && mobileMenuTrigger.classList.toggle('hidden', !isMobile());
   menu && menu.classList.toggle('hidden', isMobile());
   menuMore && menuMore.classList.toggle('hidden', !isMobile());
 };
 
 // Common
-menu && menu.addEventListener('click', e => e.stopPropagation());
-menuMore && menuMore.addEventListener('click', e => e.stopPropagation());
+menu && menu.addEventListener('click', function(e) { e.stopPropagation() });
+menuMore && menuMore.addEventListener('click', function(e) { e.stopPropagation() });
 
 isMobileMenu();
 
-document.body.addEventListener('click', () => {
+document.body.addEventListener('click', function() {
   if (!isMobile() && menuMore && !menuMore.classList.contains('hidden')) {
     menuMore && menuMore.classList.add('hidden');
   } else if (isMobile() && !menu.classList.contains('hidden')) {
@@ -30,13 +30,13 @@ document.body.addEventListener('click', () => {
 window.addEventListener('resize', isMobileMenu);
 
 // Mobile menu
-mobileMenuTrigger && mobileMenuTrigger.addEventListener('click', e => {
+mobileMenuTrigger && mobileMenuTrigger.addEventListener('click', function(e) {
   e.stopPropagation();
   menu && menu.classList.toggle('hidden');
 });
 
 // Desktop menu
-desktopMenuTrigger && desktopMenuTrigger.addEventListener('click', e => {
+desktopMenuTrigger && desktopMenuTrigger.addEventListener('click', function(e) {
   e.stopPropagation();
   menuMore && menuMore.classList.toggle('hidden');
   if (
@@ -49,18 +49,16 @@ desktopMenuTrigger && desktopMenuTrigger.addEventListener('click', e => {
 });
 
 // Toggle theme
-const getTheme = window.localStorage && window.localStorage.getItem('theme');
-const themeToggle = document.querySelector('.theme-toggle');
-const isDark = getTheme === 'dark';
+var getTheme = window.localStorage && window.localStorage.getItem('theme');
+var themeToggle = document.querySelector('.theme-toggle');
+var isDark = getTheme === 'dark';
 
 if (getTheme !== null) {
   document.body.classList.toggle('dark-theme', isDark);
 }
 
-themeToggle.addEventListener('click', () => {
+themeToggle.addEventListener('click', function() {
   document.body.classList.toggle('dark-theme');
-  window.localStorage && window.localStorage.setItem(
-    'theme',
-    document.body.classList.contains('dark-theme') ? 'dark' : 'light',
-  );
+  var theme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+  window.localStorage && window.localStorage.setItem('theme', theme);
 });
